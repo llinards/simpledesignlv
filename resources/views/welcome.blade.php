0c697fbd6dc6405d2236d7eb9c7345da93c5352c
LINARDS L <linards_lazdins@hotmail.com>
2022-02-20 19:50:47 +0200

    Initial commit & coming soon added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
+
+  <title>Simple Design</title>
+</head>
+<body class="antialiased">
+<div class="container">
+  <h1>simple design</h1>
+  <h2>coming soon</h2>
+</div>
+</body>
+</html>
